8f307819ebb1415ef0c118d53a21263516733e1f
abrordc <moh.abrorilhuda@gmail.com>
2024-06-14 14:43:05 +0700

    fix error

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,8 @@ const preloader = document.querySelector("#preloader");
 if (preloader) {
     window.addEventListener("load", () => {
         preloader.remove();
-        let tl = gsap.timeline({ defaults: { duration: 2, ease: "none" } });
-        gsap.config({ trialWarn: false });
-        tl.to("#dc", {
-            duration: 5,
-            scrambleText: {
-                text: "Kerapan sapi adalah tradisi balap sapi asal Madura,Jawa Timur, di mana dua ekor sapi menarik kereta kayu dalam perlombaan sepanjang 100 meter. Sebagai bagian dari festival adat, acara ini memiliki makna budaya mendalam dan menjadi ajang prestise bagi pemilik sapi. Sapi-sapi dilatih intensif sebelum perlombaan, dihias dengan warna cerah dan ornamen tradisional pada hari perlombaan. Penilaian pemenang didasarkan pada kecepatan dan kekompakan tim.",
-                chars: "lowerCase",
-                revealDelay: 0.5,
-                newClass: "pdc",
-                tweenLength: false
-            }
-        });
-    });
+    }) 
 }
-
 const menu = document.querySelector(".menu");
 menu.addEventListener("click", function (e) {
     const targetMenu = e.target;
