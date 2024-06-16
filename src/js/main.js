f28171b8358038a1df5e5ea4962e7b2e43a997f6
abrordc <moh.abrorilhuda@gmail.com>
2024-06-16 22:38:38 +0700

    update fitur alarm

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 const preloader = document.querySelector("#preloader");
+const dateTime = document.getElementById("time");
+const button = document.getElementById("simpan");
+const audio = document.getElementById("audio");
+audio.style.visibility = "hidden";
 if (preloader) {
     window.addEventListener("load", () => {
         preloader.remove();
-    }) 
+    });
 }
 const menu = document.querySelector(".menu");
 menu.addEventListener("click", function (e) {
@@ -28,4 +32,40 @@ window.addEventListener("blur", () => {
 
 window.addEventListener("focus", () => {
     document.title = title;
+});
+
+button.addEventListener("click", function () {
+    const combo = dateTime.value;
+    const inputUsers = new Date(combo).getTime();
+
+    const waktu = setInterval(function () {
+        const sekarang = new Date().getTime();
+        const selesai = inputUsers - sekarang;
+
+        const hari = Math.floor(selesai / (1000 * 60 * 60 * 24));
+        const jam = Math.floor(
+            (selesai % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const menit = Math.floor((selesai % (1000 * 60 * 60)) / (1000 * 60));
+        const detik = Math.floor((selesai % (1000 * 60)) / 1000);
+
+        const alarm = document.getElementById("alarm");
+        alarm.innerHTML =
+            "waktu anda tinggal : " +
+            hari +
+            " hari " +
+            jam +
+            " jam " +
+            menit +
+            " menit " +
+            detik +
+            " detik lagi! ";
+
+        if (selesai < 0) {
+            clearInterval(waktu);
+            alarm.innerHTML = "waktu anda telah habis";
+            audio.play();
+            window.reset() 
+        }
+    }, 1000);
 });
